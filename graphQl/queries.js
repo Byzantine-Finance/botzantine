36e332377b171aeed72a5668f63694cb81568913
asanson <alexandresanson.pro@gmail.com>
2024-10-30 01:35:00 +0800

    feat: Add graphql-request to fetch the Graph

---
+++ graphQl/queries.js
@@ -0,0 +1,18 @@
+export const GetNewCreatedDVs = `query GetCreatedDVs($timestamp: BigInt!) {
+    clusterCreateds(
+        where: { timestamp_gt: $timestamp }
+        orderBy: timestamp
+        orderDirection: desc
+    ) {
+        id
+        timestamp
+        txHash
+        splitAddress
+        eigenPodAddr
+        winners {
+            nodeOp {
+                nodeOpAddr
+            }
+        }
+    }
+}`;
